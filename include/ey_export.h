81f5df97c513f72a2a68a730b9020bed2a7f2ecb
eyoung-father <eyoung.father@gmail.com>
2013-09-15 19:24:46 -0700

    less export section

--- include/ey_export.h
+++ include/ey_export.h
@@ -2,18 +2,14 @@
 #define EY_EXPORT_H 1
 
 #define EY_EXPORT_TYPE_IDENT			1
-#define EY_EXPORT_TYPE_DYNAMIC_IDENT	2
-#define EY_EXPORT_TYPE_TYPE				3
-#define EY_EXPORT_TYPE_INIT				4
-#define EY_EXPORT_TYPE_ENTER			5
-#define EY_EXPORT_TYPE_EXIT				6
+#define EY_EXPORT_TYPE_TYPE				2
+#define EY_EXPORT_TYPE_INIT				3
+#define EY_EXPORT_TYPE_FINIT			4
 
 #define EY_IDENT_SECTION				".eyoung_ident"
-#define EY_DYNAMIC_IDENT_SECTION		".eyoung_dynamic"
 #define EY_TYPE_SECTION					".eyoung_type"
 #define EY_INIT_SECTION					".eyoung_init"
-#define EY_ENTER_SECTION				".eyoung_enter"
-#define EY_EXIT_SECTION					".eyoung_exit"
+#define EY_FINIT_SECTION				".eyoung_finit"
 
 typedef struct ey_extern_symbol
 {
@@ -35,18 +31,6 @@ typedef struct ey_extern_symbol
 		__FILE__, 												\
 		__LINE__, 												\
 		EY_EXPORT_TYPE_IDENT									\
-	};
-
-#define EY_EXPORT_DYNAMIC_IDENT(name, decl)						\
-	static const ey_extern_symbol_t __eyoung_ident_##name		\
-	__attribute__((section(EY_DYNAMIC_IDENT_SECTION), unused)) =\
-	{ 															\
-		#name,													\
-		(void*)0, 												\
-		decl, 													\
-		__FILE__, 												\
-		__LINE__, 												\
-		EY_EXPORT_TYPE_DYNAMIC_IDENT							\
 	};
 
 #define EY_EXPORT_TYPE(name,decl)								\
@@ -73,27 +57,15 @@ typedef struct ey_extern_symbol
 		EY_EXPORT_TYPE_INIT										\
 	};
 
-#define EY_EXPORT_ENTER(name)									\
+#define EY_EXPORT_FINIT(name)									\
 	static const ey_extern_symbol_t __eyoung_type_##name		\
-	__attribute__((section(EY_ENTER_SECTION), unused)) = 		\
+	__attribute__((section(EY_FINIT_SECTION), unused)) = 		\
 	{															\
 		#name, 													\
 		name, 													\
 		(void*)0, 												\
 		__FILE__, 												\
 		__LINE__,												\
-		EY_EXPORT_TYPE_ENTER									\
-	};
-
-#define EY_EXPORT_EXIT(name)									\
-	static const ey_extern_symbol_t __eyoung_type_##name		\
-	__attribute__((section(EY_EXIT_SECTION), unused)) = 		\
-	{															\
-		#name, 													\
-		name, 													\
-		(void*)0, 												\
-		__FILE__, 												\
-		__LINE__,												\
-		EY_EXPORT_TYPE_EXIT										\
+		EY_EXPORT_TYPE_FINIT									\
 	};
 #endif 
