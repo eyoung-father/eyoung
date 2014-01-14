5b09e572d9dbe4deda7e15ccaf85fbe1553a1556
eyoung-father <eyoung.father@gmail.com>
2014-01-14 08:23:18 -0800

    http type

--- libdecoder/http/include/http_type.h
+++ libdecoder/http/include/http_type.h
@@ -8,36 +8,546 @@
 /*
  * Client Message Type
  */
-typedef int http_request_type_t;
+typedef enum http_request_method
+{
+	HTTP_REQUEST_METHOD_METHOD_GET = 1,
+	HTTP_REQUEST_METHOD_METHOD_POST,
+	HTTP_REQUEST_METHOD_METHOD_HEAD,
+	HTTP_REQUEST_METHOD_METHOD_OPTIONS,
+	HTTP_REQUEST_METHOD_METHOD_PUT,
+	HTTP_REQUEST_METHOD_METHOD_DELETE,
+	HTTP_REQUEST_METHOD_METHOD_TRACE,
+	HTTP_REQUEST_METHOD_METHOD_CONNECT,
+	HTTP_REQUEST_METHOD_METHOD_PATCH,
+	HTTP_REQUEST_METHOD_METHOD_LINK,
+	HTTP_REQUEST_METHOD_METHOD_UNLINK,
+	HTTP_REQUEST_METHOD_METHOD_PROPFIND,
+	HTTP_REQUEST_METHOD_METHOD_PROPPATCH,
+	HTTP_REQUEST_METHOD_METHOD_MKCOL,
+	HTTP_REQUEST_METHOD_METHOD_COPY,
+	HTTP_REQUEST_METHOD_METHOD_MOVE,
+	HTTP_REQUEST_METHOD_METHOD_LOCK,
+	HTTP_REQUEST_METHOD_METHOD_UNLOCK,
+	HTTP_REQUEST_METHOD_METHOD_CHECKOUT,
+	HTTP_REQUEST_METHOD_METHOD_REPORT,
+	HTTP_REQUEST_METHOD_METHOD_VERSION_CONTROL,
+	HTTP_REQUEST_METHOD_METHOD_CHECKIN,
+	HTTP_REQUEST_METHOD_METHOD_UNCHECKOUT,
+	HTTP_REQUEST_METHOD_METHOD_MKWORKSPACE,
+	HTTP_REQUEST_METHOD_METHOD_UPDATE,
+	HTTP_REQUEST_METHOD_METHOD_LABEL,
+	HTTP_REQUEST_METHOD_METHOD_MERGE,
+	HTTP_REQUEST_METHOD_METHOD_MKACTIVITY,
+	HTTP_REQUEST_METHOD_METHOD_ORDERPATCH,
+	HTTP_REQUEST_METHOD_METHOD_BASELINE_CONTROL,
+
+	HTTP_REQUEST_METHOD_METHOD_UNKOWN
+}http_request_method_t;
+
+static inline const char *http_request_method_name(http_request_method_t method)
+{
+	switch(method)
+	{
+		case HTTP_REQUEST_METHOD_METHOD_GET:
+			return "GET";
+		case HTTP_REQUEST_METHOD_METHOD_POST:
+			return "POST";
+		case HTTP_REQUEST_METHOD_METHOD_HEAD:
+			return "HEAD";
+		case HTTP_REQUEST_METHOD_METHOD_OPTIONS:
+			return "OPTIONS";
+		case HTTP_REQUEST_METHOD_METHOD_PUT:
+			return "PUT";
+		case HTTP_REQUEST_METHOD_METHOD_DELETE:
+			return "DELETE";
+		case HTTP_REQUEST_METHOD_METHOD_TRACE:
+			return "TRACE";
+		case HTTP_REQUEST_METHOD_METHOD_CONNECT:
+			return "CONNECT";
+		case HTTP_REQUEST_METHOD_METHOD_PATCH:
+			return "PATCH";
+		case HTTP_REQUEST_METHOD_METHOD_LINK:
+			return "LINK";
+		case HTTP_REQUEST_METHOD_METHOD_UNLINK:
+			return "UNLINK";
+		case HTTP_REQUEST_METHOD_METHOD_PROPFIND:
+			return "PROPFIND";
+		case HTTP_REQUEST_METHOD_METHOD_PROPPATCH:
+			return "PROPPATCH";
+		case HTTP_REQUEST_METHOD_METHOD_MKCOL:
+			return "MKCOL";
+		case HTTP_REQUEST_METHOD_METHOD_COPY:
+			return "COPY";
+		case HTTP_REQUEST_METHOD_METHOD_MOVE:
+			return "MOVE";
+		case HTTP_REQUEST_METHOD_METHOD_LOCK:
+			return "LOCK";
+		case HTTP_REQUEST_METHOD_METHOD_UNLOCK:
+			return "UNLOCK";
+		case HTTP_REQUEST_METHOD_METHOD_CHECKOUT:
+			return "CHECKOUT";
+		case HTTP_REQUEST_METHOD_METHOD_REPORT:
+			return "REPORT";
+		case HTTP_REQUEST_METHOD_METHOD_VERSION_CONTROL:
+			return "VERSION-CONTROL";
+		case HTTP_REQUEST_METHOD_METHOD_CHECKIN:
+			return "CHECKIN";
+		case HTTP_REQUEST_METHOD_METHOD_UNCHECKOUT:
+			return "UNCHECKOUT";
+		case HTTP_REQUEST_METHOD_METHOD_MKWORKSPACE:
+			return "MKWORKSPACE";
+		case HTTP_REQUEST_METHOD_METHOD_UPDATE:
+			return "UPDATE";
+		case HTTP_REQUEST_METHOD_METHOD_LABEL:
+			return "LABEL";
+		case HTTP_REQUEST_METHOD_METHOD_MERGE:
+			return "MERGE";
+		case HTTP_REQUEST_METHOD_METHOD_MKACTIVITY:
+			return "MKACTIVITY";
+		case HTTP_REQUEST_METHOD_METHOD_ORDERPATCH:
+			return "ORDERPATCH";
+		case HTTP_REQUEST_METHOD_METHOD_BASELINE_CONTROL:
+			return "BASELINE-CONTROL";
+		case HTTP_REQUEST_METHOD_METHOD_UNKOWN:
+		default:
+			return "(UNKOWN)";
+	}
+}
+
+typedef enum http_request_version
+{
+	HTTP_REQUEST_VERSION_09,
+	HTTP_REQUEST_VERSION_10,
+	HTTP_REQUEST_VERSION_11,
+
+	HTTP_REQUEST_VERSION_UNKOWN
+}http_request_version_t;
+
+static inline const char* http_request_version_name(http_request_version_t version)
+{
+	switch(version)
+	{
+		case HTTP_REQUEST_VERSION_09:
+			return "HTTP/0.9";
+		case HTTP_REQUEST_VERSION_10:
+			return "HTTP/1.0";
+		case HTTP_REQUEST_VERSION_11:
+			return "HTTP/1.1";
+		case HTTP_REQUEST_VERSION_UNKOWN:
+		default:
+			return "(UNKOWN)";
+	}
+}
+
+typedef struct http_request_first_line
+{
+	http_request_method_t method;
+	http_request_version_t version;
+	char *uri;
+}http_request_first_line_t;
+
+typedef enum http_request_header_type
+{
+	HTTP_REQUEST_HEADER_HOST,
+	HTTP_REQUEST_HEADER_CACHE_CONTROL,
+	HTTP_REQUEST_HEADER_CONNECTION,
+	HTTP_REQUEST_HEADER_DATE,
+	HTTP_REQUEST_HEADER_PRAGMA,
+	HTTP_REQUEST_HEADER_TRAILER, 
+	HTTP_REQUEST_HEADER_TRANSFER_ENCODING,
+	HTTP_REQUEST_HEADER_UPGRADE,
+	HTTP_REQUEST_HEADER_VIA,
+	HTTP_REQUEST_HEADER_WARNING,
+	HTTP_REQUEST_HEADER_MIME_VERSION,
+	HTTP_REQUEST_HEADER_ALLOW,
+	HTTP_REQUEST_HEADER_CONTENT_ENCODING,
+	HTTP_REQUEST_HEADER_CONTENT_LANGUAGE,
+	HTTP_REQUEST_HEADER_CONTENT_LENGTH,
+	HTTP_REQUEST_HEADER_CONTENT_LOCATION,
+	HTTP_REQUEST_HEADER_CONTENT_MD5,
+	HTTP_REQUEST_HEADER_CONTENT_RANGE,
+	HTTP_REQUEST_HEADER_CONTENT_TYPE,
+	HTTP_REQUEST_HEADER_ETAG,
+	HTTP_REQUEST_HEADER_EXPIRES,
+	HTTP_REQUEST_HEADER_LAST_MODIFIED,
+	HTTP_REQUEST_HEADER_CONTENT_BASE,
+	HTTP_REQUEST_HEADER_CONTENT_VERSION,
+	HTTP_REQUEST_HEADER_DERIVED_FROM,
+	HTTP_REQUEST_HEADER_LINK,
+	HTTP_REQUEST_HEADER_KEEP_ALIVE,
+	HTTP_REQUEST_HEADER_URI,
+	HTTP_REQUEST_HEADER_ACCEPT_CHARSET,
+	HTTP_REQUEST_HEADER_ACCEPT_ENCODING,
+	HTTP_REQUEST_HEADER_ACCEPT_LANGUAGE,
+	HTTP_REQUEST_HEADER_ACCEPT,
+	HTTP_REQUEST_HEADER_AUTHORIZATION,
+	HTTP_REQUEST_HEADER_EXCEPT,
+	HTTP_REQUEST_HEADER_FROM,
+	HTTP_REQUEST_HEADER_IF_MATCH,
+	HTTP_REQUEST_HEADER_IF_MODIFIED_SINCE,
+	HTTP_REQUEST_HEADER_IF_NONE_MATCH,
+	HTTP_REQUEST_HEADER_IF_RANGE,
+	HTTP_REQUEST_HEADER_IF_UNMODIFIED_SINCE,
+	HTTP_REQUEST_HEADER_MAX_FORWARDS,
+	HTTP_REQUEST_HEADER_RANGE,
+	HTTP_REQUEST_HEADER_REFERER,
+	HTTP_REQUEST_HEADER_TE,
+	HTTP_REQUEST_HEADER_USER_AGENT,
+	HTTP_REQUEST_HEADER_COOKIE2,
+	HTTP_REQUEST_HEADER_COOKIE,
+	HTTP_REQUEST_HEADER_UA_PIXELS,
+	HTTP_REQUEST_HEADER_UA_COLOR,
+	HTTP_REQUEST_HEADER_UA_OS,
+	HTTP_REQUEST_HEADER_UA_CPU,
+	HTTP_REQUEST_HEADER_X_FLASH_VERSION,
+	HTTP_REQUEST_HEADER_PROXY_AUTHORIZATION,
+
+	HTTP_REQUEST_HEADER_UNKOWN
+}http_request_header_type_t;
+
+static inline const char* http_request_header_name(http_request_header_type_t type)
+{
+	switch(type)
+	{
+		case HTTP_REQUEST_HEADER_HOST:
+			return "HOST";
+		case HTTP_REQUEST_HEADER_CACHE_CONTROL:
+			return "CACHE-CONTROL";
+		case HTTP_REQUEST_HEADER_CONNECTION:
+			return "CONNECTION";
+		case HTTP_REQUEST_HEADER_DATE:
+			return "DATE";
+		case HTTP_REQUEST_HEADER_PRAGMA:
+			return "PRAGMA";
+		case HTTP_REQUEST_HEADER_TRAILER:
+			return "TRAILER";
+		case HTTP_REQUEST_HEADER_TRANSFER_ENCODING:
+			return "TRANSFER-ENCODING";
+		case HTTP_REQUEST_HEADER_UPGRADE:
+			return "UPGRADE";
+		case HTTP_REQUEST_HEADER_VIA:
+			return "VIA";
+		case HTTP_REQUEST_HEADER_WARNING:
+			return "WARNING";
+		case HTTP_REQUEST_HEADER_MIME_VERSION:
+			return "MIME-VERSION";
+		case HTTP_REQUEST_HEADER_ALLOW:
+			return "ALLOW";
+		case HTTP_REQUEST_HEADER_CONTENT_ENCODING:
+			return "CONTENT-ENCODING";
+		case HTTP_REQUEST_HEADER_CONTENT_LANGUAGE:
+			return "CONTENT-LANGUAGE";
+		case HTTP_REQUEST_HEADER_CONTENT_LENGTH:
+			return "CONTENT-LENGTH";
+		case HTTP_REQUEST_HEADER_CONTENT_LOCATION:
+			return "CONTENT-LOCATION";
+		case HTTP_REQUEST_HEADER_CONTENT_MD5:
+			return "CONTENT-MD5";
+		case HTTP_REQUEST_HEADER_CONTENT_RANGE:
+			return "CONTENT-RANGE";
+		case HTTP_REQUEST_HEADER_CONTENT_TYPE:
+			return "CONTENT-TYPE";
+		case HTTP_REQUEST_HEADER_ETAG:
+			return "ETAG";
+		case HTTP_REQUEST_HEADER_EXPIRES:
+			return "EXPIRES";
+		case HTTP_REQUEST_HEADER_LAST_MODIFIED:
+			return "LAST-MODIFIED";
+		case HTTP_REQUEST_HEADER_CONTENT_BASE:
+			return "CONTENT-BASE";
+		case HTTP_REQUEST_HEADER_CONTENT_VERSION:
+			return "CONTENT-VERSION";
+		case HTTP_REQUEST_HEADER_DERIVED_FROM:
+			return "DERIVED-FROM";
+		case HTTP_REQUEST_HEADER_LINK:
+			return "LINK";
+		case HTTP_REQUEST_HEADER_KEEP_ALIVE:
+			return "KEEP-ALIVE";
+		case HTTP_REQUEST_HEADER_URI:
+			return "URI";
+		case HTTP_REQUEST_HEADER_ACCEPT_CHARSET:
+			return "ACCEPT-CHARSET";
+		case HTTP_REQUEST_HEADER_ACCEPT_ENCODING:
+			return "ACCEPT-ENCODING";
+		case HTTP_REQUEST_HEADER_ACCEPT_LANGUAGE:
+			return "ACCEPT-LANGUAGE";
+		case HTTP_REQUEST_HEADER_ACCEPT:
+			return "ACCEPT";
+		case HTTP_REQUEST_HEADER_AUTHORIZATION:
+			return "AUTHORIZATION";
+		case HTTP_REQUEST_HEADER_EXCEPT:
+			return "EXCEPT";
+		case HTTP_REQUEST_HEADER_FROM:
+			return "FROM";
+		case HTTP_REQUEST_HEADER_IF_MATCH:
+			return "IF-MATCH";
+		case HTTP_REQUEST_HEADER_IF_MODIFIED_SINCE:
+			return "IF-MODIFIED-SINCE";
+		case HTTP_REQUEST_HEADER_IF_NONE_MATCH:
+			return "IF-NONE-MATCH";
+		case HTTP_REQUEST_HEADER_IF_RANGE:
+			return "IF-RANGE";
+		case HTTP_REQUEST_HEADER_IF_UNMODIFIED_SINCE:
+			return "IF-UNMODIFIED-SINCE";
+		case HTTP_REQUEST_HEADER_MAX_FORWARDS:
+			return "MAX-FORWARDS";
+		case HTTP_REQUEST_HEADER_RANGE:
+			return "RANGE";
+		case HTTP_REQUEST_HEADER_REFERER:
+			return "REFERER";
+		case HTTP_REQUEST_HEADER_TE:
+			return "TE";
+		case HTTP_REQUEST_HEADER_USER_AGENT:
+			return "USER-AGENT";
+		case HTTP_REQUEST_HEADER_COOKIE2:
+			return "COOKIE2";
+		case HTTP_REQUEST_HEADER_COOKIE:
+			return "COOKIE";
+		case HTTP_REQUEST_HEADER_UA_PIXELS:
+			return "UA-PIXELS";
+		case HTTP_REQUEST_HEADER_UA_COLOR:
+			return "UA-COLOR";
+		case HTTP_REQUEST_HEADER_UA_OS:
+			return "UA-OS";
+		case HTTP_REQUEST_HEADER_UA_CPU:
+			return "UA-CPU";
+		case HTTP_REQUEST_HEADER_X_FLASH_VERSION:
+			return "X-FLASH-VERSION";
+		case HTTP_REQUEST_HEADER_PROXY_AUTHORIZATION:
+			return "PROXY-AUTHORIZATION";
+		case HTTP_REQUEST_HEADER_UNKOWN:
+		default:
+			return "(UNKOWN)";
+	}
+}
+
+typedef struct http_request_header
+{
+	http_request_header_type_t type;
+	char *value;
+	STAILQ_ENTRY(http_request_header) next;
+}http_request_header_t;
+typedef STAILQ_HEAD(http_request_header_list, http_request_header) http_request_header_list_t;
+
+typedef struct http_request_body_part
+{
+	char *value;
+	STAILQ_ENTRY(http_request_body_part) next;
+}http_request_body_part_t;
+typedef STAILQ_HEAD(http_request_body, http_request_body_part) http_request_body_t;
 
 typedef struct http_request
 {
-	http_request_type_t req_code;
+	http_request_first_line_t first_line;
+	http_request_header_list_t header_list;
+	http_request_body_t body;
+
 	STAILQ_ENTRY(http_request) next;
 }http_request_t;
 
-typedef STAILQ_HEAD(http_requests, http_request) http_requests_t;
+typedef STAILQ_HEAD(http_request_list, http_request) http_request_list_t;
 
 /*
  * Server Message Type
  */
-typedef int http_response_type_t;
+typedef enum http_response_version
+{
+	HTTP_RESPONSE_VERSION_09,
+	HTTP_RESPONSE_VERSION_10,
+	HTTP_RESPONSE_VERSION_11,
+
+	HTTP_RESPOSNE_VERSION_UNKOWN
+}http_response_version_t;
+
+static inline const char* http_resposne_version_name(http_response_version_t version)
+{
+	switch(version)
+	{
+		case HTTP_RESPONSE_VERSION_09:
+			return "HTTP/0.9";
+		case HTTP_RESPONSE_VERSION_10:
+			return "HTTP/1.0";
+		case HTTP_RESPONSE_VERSION_11:
+			return "HTTP/1.1";
+		case HTTP_RESPOSNE_VERSION_UNKOWN:
+		default:
+			return "(UNKOWN)";
+	}
+}
+
+typedef int http_response_code_t;
+
+typedef struct http_response_first_line
+{
+	http_response_version_t version;
+	http_response_code_t code;
+	char *message;
+}http_response_first_line_t;
+
+typedef enum http_response_header_type
+{
+	HTTP_RESPONSE_HEADER_CACHE_CONTROL,
+	HTTP_RESPONSE_HEADER_CONNECTION,
+	HTTP_RESPONSE_HEADER_DATE,
+	HTTP_RESPONSE_HEADER_PRAGMA,
+	HTTP_RESPONSE_HEADER_TRAILER,
+	HTTP_RESPONSE_HEADER_TRANSFER_ENCODING,
+	HTTP_RESPONSE_HEADER_UPGRADE,
+	HTTP_RESPONSE_HEADER_VIA,
+	HTTP_RESPONSE_HEADER_WARNING,
+	HTTP_RESPONSE_HEADER_MIME_VERSION,
+	HTTP_RESPONSE_HEADER_ALLOW,
+	HTTP_RESPONSE_HEADER_CONTENT_ENCODING,
+	HTTP_RESPONSE_HEADER_CONTENT_LANGUAGE,
+	HTTP_RESPONSE_HEADER_CONTENT_LENGTH,
+	HTTP_RESPONSE_HEADER_CONTENT_LOCATION,
+	HTTP_RESPONSE_HEADER_CONTENT_MD5,
+	HTTP_RESPONSE_HEADER_CONTENT_RANGE,
+	HTTP_RESPONSE_HEADER_CONTENT_TYPE,
+	HTTP_RESPONSE_HEADER_ETAG,
+	HTTP_RESPONSE_HEADER_EXPIRES,
+	HTTP_RESPONSE_HEADER_LAST_MODIFIED,
+	HTTP_RESPONSE_HEADER_CONTENT_BASE,
+	HTTP_RESPONSE_HEADER_CONTENT_VERSION,
+	HTTP_RESPONSE_HEADER_DERIVED_FROM,
+	HTTP_RESPONSE_HEADER_LINK,
+	HTTP_RESPONSE_HEADER_KEEP_ALIVE,
+	HTTP_RESPONSE_HEADER_URI,
+	HTTP_RESPONSE_HEADER_ACCEPT_RANGES,
+	HTTP_RESPONSE_HEADER_AGE,
+	HTTP_RESPONSE_HEADER_LOCATION,
+	HTTP_RESPONSE_HEADER_RETRY_AFTER,
+	HTTP_RESPONSE_HEADER_SERVER,
+	HTTP_RESPONSE_HEADER_VARY,
+	HTTP_RESPONSE_HEADER_WWW_AUTHENTICATE,
+	HTTP_RESPONSE_HEADER_SET_COOKIE2,
+	HTTP_RESPONSE_HEADER_SET_COOKIE,
+	HTTP_RESPONSE_HEADER_X_POWERED_BY,
+	HTTP_RESPONSE_HEADER_PROXY_AUTHENTICATE,
+	HTTP_RESPONSE_HEADER_UNKOWN
+}http_response_header_type_t;
+
+static inline const char* http_response_header_name(http_response_header_type_t type)
+{
+	switch(type)
+	{
+		case HTTP_RESPONSE_HEADER_CACHE_CONTROL:
+			return "CACHE-CONTROL";
+		case HTTP_RESPONSE_HEADER_CONNECTION:
+			return "CONNECTION";
+		case HTTP_RESPONSE_HEADER_DATE:
+			return "DATE";
+		case HTTP_RESPONSE_HEADER_PRAGMA:
+			return "PRAGMA";
+		case HTTP_RESPONSE_HEADER_TRAILER:
+			return "TRAILER";
+		case HTTP_RESPONSE_HEADER_TRANSFER_ENCODING:
+			return "TRANSFER-ENCODING";
+		case HTTP_RESPONSE_HEADER_UPGRADE:
+			return "UPGRADE";
+		case HTTP_RESPONSE_HEADER_VIA:
+			return "VIA";
+		case HTTP_RESPONSE_HEADER_WARNING:
+			return "WARNING";
+		case HTTP_RESPONSE_HEADER_MIME_VERSION:
+			return "MIME-VERSION";
+		case HTTP_RESPONSE_HEADER_ALLOW:
+			return "ALLOW";
+		case HTTP_RESPONSE_HEADER_CONTENT_ENCODING:
+			return "CONTENT-ENCODING";
+		case HTTP_RESPONSE_HEADER_CONTENT_LANGUAGE:
+			return "CONTENT-LANGUAGE";
+		case HTTP_RESPONSE_HEADER_CONTENT_LENGTH:
+			return "CONTENT-LENGTH";
+		case HTTP_RESPONSE_HEADER_CONTENT_LOCATION:
+			return "CONTENT-LOCATION";
+		case HTTP_RESPONSE_HEADER_CONTENT_MD5:
+			return "CONTENT-MD5";
+		case HTTP_RESPONSE_HEADER_CONTENT_RANGE:
+			return "CONTENT-RANGE";
+		case HTTP_RESPONSE_HEADER_CONTENT_TYPE:
+			return "CONTENT-TYPE";
+		case HTTP_RESPONSE_HEADER_ETAG:
+			return "ETAG";
+		case HTTP_RESPONSE_HEADER_EXPIRES:
+			return "EXPIRES";
+		case HTTP_RESPONSE_HEADER_LAST_MODIFIED:
+			return "LAST-MODIFIED";
+		case HTTP_RESPONSE_HEADER_CONTENT_BASE:
+			return "CONTENT-BASE";
+		case HTTP_RESPONSE_HEADER_CONTENT_VERSION:
+			return "CONTENT-VERSION";
+		case HTTP_RESPONSE_HEADER_DERIVED_FROM:
+			return "DERIVED-FROM";
+		case HTTP_RESPONSE_HEADER_LINK:
+			return "LINK";
+		case HTTP_RESPONSE_HEADER_KEEP_ALIVE:
+			return "KEEP-ALIVE";
+		case HTTP_RESPONSE_HEADER_URI:
+			return "URI";
+		case HTTP_RESPONSE_HEADER_ACCEPT_RANGES:
+			return "ACCEPT-RANGES";
+		case HTTP_RESPONSE_HEADER_AGE:
+			return "AGE";
+		case HTTP_RESPONSE_HEADER_LOCATION:
+			return "LOCATION";
+		case HTTP_RESPONSE_HEADER_RETRY_AFTER:
+			return "RETRY-AFTER";
+		case HTTP_RESPONSE_HEADER_SERVER:
+			return "SERVER";
+		case HTTP_RESPONSE_HEADER_VARY:
+			return "VARY";
+		case HTTP_RESPONSE_HEADER_WWW_AUTHENTICATE:
+			return "WWW-AUTHENTICATE";
+		case HTTP_RESPONSE_HEADER_SET_COOKIE2:
+			return "SET-COOKIE2";
+		case HTTP_RESPONSE_HEADER_SET_COOKIE:
+			return "SET-COOKIE";
+		case HTTP_RESPONSE_HEADER_X_POWERED_BY:
+			return "X-POWERED-BY";
+		case HTTP_RESPONSE_HEADER_PROXY_AUTHENTICATE:
+			return "PROXY-AUTHENTICATE";
+		case HTTP_RESPONSE_HEADER_UNKOWN:
+		default:
+			return "(UNKOWN)";
+	}
+}
+
+typedef struct http_response_header
+{
+	http_response_header_type_t type;
+	char *value;
+
+	STAILQ_ENTRY(http_response_header) next;
+}http_response_header_t;
+typedef STAILQ_HEAD(http_resposne_header_list, http_response_header) http_response_header_list_t;
+
+typedef struct http_response_body_part
+{
+	char *value;
+	STAILQ_ENTRY(http_response_body_part) next;
+}http_response_body_part_t;
+typedef STAILQ_HEAD(http_response_body, http_response_body_part) http_response_body_t;
 
 typedef struct http_response
 {
-	http_response_type_t res_code;
+	http_response_first_line_t first_line;
+	http_response_header_list_t header_list;
+	http_response_body_t body;
+
 	STAILQ_ENTRY(http_response) next;
 }http_response_t;
 
-typedef STAILQ_HEAD(http_responses, http_response) http_responses_t;
+typedef STAILQ_HEAD(http_response_list, http_response) http_response_list_t;
 
 /*
  * http session command
  */
 typedef struct http_cmd
 {
-	http_request_t *req;
-	http_response_t *res;
+	http_request_t *request;
+	http_response_t *response;
+
 	STAILQ_ENTRY(http_cmd) next;
 }http_cmd_t;
 
@@ -67,11 +577,11 @@ typedef struct http_data
 
 	/*client ==> server*/
 	http_parser_t request_parser;
-	http_requests_t request_list;
+	http_request_list_t request_list;
 
 	/*server ==> client*/
 	http_parser_t response_parser;
-	http_responses_t response_list;
+	http_response_list_t response_list;
 
 	/*http session*/
 	http_cmd_list_t cmd_list;
