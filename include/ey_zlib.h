ded789b30ad72d0fadcb353b72a4441221203ada
eyoung-father <eyoung.father@gmail.com>
2014-02-21 23:53:47 -0800

    ey_zlib create/destroy/errmsg function

--- include/ey_zlib.h
+++ include/ey_zlib.h
@@ -1,4 +1,24 @@
 #ifndef EY_ZLIB_H
 #define EY_ZLIB_H 1
 
+typedef void* ey_zlib_t;
+
+typedef enum ey_zlib_format
+{
+	EY_ZLIB_FORMAT_GZIP_PACK,
+	EY_ZLIB_FORMAT_GZIP_UNPACK,
+	EY_ZLIB_FORMAT_DEFLATE_PACK,
+	EY_ZLIB_FORMAT_DEFLATE_UNPACK,
+
+	EY_ZLIB_FORMAT_UNKOWN
+}ey_zlib_format_t;
+
+struct memory_handler;
+extern ey_zlib_t ey_zlib_create(struct memory_handler *mm, size_t ilen, size_t olen, ey_zlib_format_t fmt, void *arg);
+extern void ey_zlib_destroy(ey_zlib_t z);
+
+typedef int (*ey_zlib_callback)(ey_zlib_t z, char *o_buf, size_t o_len, void *arg);
+extern int ey_zlib_pack(ey_zlib_t z, char *i_buf, size_t i_len, ey_zlib_callback cb, void *arg);
+extern int ey_zlib_unpack(ey_zlib_t z, char *i_buf, size_t i_len, ey_zlib_callback cb, void *arg);
+extern const char *ey_zlib_errstr(ey_zlib_t z);
 #endif
---
+++ libz/ey_zlib.c
@@ -0,0 +1,196 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+#include <stdarg.h>
+
+#include "ey_memory.h"
+#include "ey_zlib.h"
+#include "zlib.h"
+
+#define DEFAULT_INPUT_LENGTH	1024
+#define DEFAULT_OUTPUT_LENGTH	2048
+typedef struct ey_zlib_private
+{
+	memory_handler_t memory_handler;
+	ey_zlib_format_t format;
+	size_t input_length;
+	size_t output_length;
+	void *create_arg;
+	void *runtime_arg;
+	char *err_msg;
+	z_stream zstream;
+}ey_zlib_private_t;
+
+static ey_slab_t zlib_slab;
+static int debug_zlib_basic = 1;
+
+static void zlib_debug(int flag, const char *format, ...)
+{
+	if(!flag)
+		return;
+	
+	va_list ap;
+	va_start(ap, format);
+	vfprintf(stderr, format, ap);
+	va_end(ap);
+}
+
+static void *ey_zlib_alloc(void *arg, unsigned int n, unsigned int size)
+{
+	assert(arg != NULL);
+	ey_zlib_private_t *priv_data = (ey_zlib_private_t*)arg;
+	size_t total_len = n * size;
+	return priv_data->memory_handler.malloc(total_len);
+}
+
+static void ey_zlib_free(void *arg, void *ptr)
+{
+	assert(arg != NULL);
+	ey_zlib_private_t *priv_data = (ey_zlib_private_t*)arg;
+	if(ptr)
+		priv_data->memory_handler.free(ptr);
+}
+
+ey_zlib_t ey_zlib_create(memory_handler_t *mm, size_t ilen, size_t olen, ey_zlib_format_t fmt, void *arg)
+{
+	memory_handler_t mm_handler = {ey_malloc,ey_realloc,ey_free,ey_calloc};
+	char *i_buf = NULL;
+	char *o_buf = NULL;
+	ey_zlib_private_t *ret = NULL;
+	z_stream *sp = NULL;
+	
+	if(!mm)
+		mm = &mm_handler;
+	if(ilen < DEFAULT_INPUT_LENGTH)
+		ilen = DEFAULT_INPUT_LENGTH;
+	if(olen < DEFAULT_OUTPUT_LENGTH)
+		olen = DEFAULT_OUTPUT_LENGTH;
+	
+	assert(	fmt==EY_ZLIB_FORMAT_GZIP_PACK		|| 
+			fmt==EY_ZLIB_FORMAT_GZIP_UNPACK		|| 
+			fmt==EY_ZLIB_FORMAT_DEFLATE_PACK	||
+			fmt==EY_ZLIB_FORMAT_DEFLATE_UNPACK);
+
+	if(!zlib_slab)
+	{
+		zlib_slab = ey_zinit("ey_zlib_slab", sizeof(ey_zlib_private_t), NULL);
+		if(!zlib_slab)
+		{
+			zlib_debug(debug_zlib_basic, "zlib slab create failed\n");
+			goto failed;
+		}
+	}
+
+	ret = (ey_zlib_private_t*)ey_zalloc(zlib_slab);
+	if(!ret)
+	{
+		zlib_debug(debug_zlib_basic, "alloc zlib_private data failed\n");
+		goto failed;
+	}
+	memset(ret, 0, sizeof(ey_zlib_private_t));
+	ret->memory_handler = *mm;
+	ret->create_arg = arg;
+
+	sp = &ret->zstream;
+	sp->zalloc = ey_zlib_alloc;
+	sp->zfree = ey_zlib_free;
+	sp->opaque = ret;
+	/*TODO: zstream init*/
+	ret->format = fmt;
+
+	i_buf = (char*)mm->malloc(ilen);
+	if(!i_buf)
+	{
+		zlib_debug(debug_zlib_basic, "alloc input buffer failed\n");
+		goto failed;
+	}
+	ret->input_length = ilen;
+	sp->next_in = i_buf;
+	sp->avail_in = 0;
+
+	o_buf = (char*)mm->malloc(olen);
+	if(!o_buf)
+	{
+		zlib_debug(debug_zlib_basic, "alloc output buffer failed\n");
+		goto failed;
+	}
+	ret->output_length = olen;
+	sp->next_out = o_buf;
+	sp->avail_out = olen;
+	
+	/*now we can return successfully*/
+	zlib_debug(debug_zlib_basic, "create ey_zlib successfully\n");
+	return ret;
+
+failed:
+	if(sp)
+	{
+		if(fmt==EY_ZLIB_FORMAT_GZIP_PACK || fmt==EY_ZLIB_FORMAT_DEFLATE_PACK)
+			deflateEnd(sp);
+		if(fmt==EY_ZLIB_FORMAT_GZIP_UNPACK || fmt==EY_ZLIB_FORMAT_DEFLATE_UNPACK)
+			inflateEnd(sp);
+	}
+
+	if(o_buf)
+		mm->free(o_buf);
+
+	if(i_buf)
+		mm->free(i_buf);
+
+	if(ret)
+		ey_zfree(zlib_slab, ret);
+	
+	return NULL;
+}
+
+void ey_zlib_destroy(ey_zlib_t z)
+{
+	if(!z)
+		return;
+	
+	ey_zlib_private_t *priv_data = (ey_zlib_private_t*)z;
+	memory_handler_t *mm = &priv_data->memory_handler;
+	z_stream *sp = &priv_data->zstream;
+
+	char *i_buf = sp->next_in;
+	char *o_buf = sp->next_out;
+	assert(i_buf != NULL && o_buf != NULL);
+
+	ey_zlib_format_t fmt = priv_data->format;
+	assert(	fmt==EY_ZLIB_FORMAT_GZIP_PACK		|| 
+			fmt==EY_ZLIB_FORMAT_GZIP_UNPACK		|| 
+			fmt==EY_ZLIB_FORMAT_DEFLATE_PACK	||
+			fmt==EY_ZLIB_FORMAT_DEFLATE_UNPACK);
+	
+	if(fmt==EY_ZLIB_FORMAT_GZIP_PACK || fmt==EY_ZLIB_FORMAT_DEFLATE_PACK)
+		deflateEnd(sp);
+	if(fmt==EY_ZLIB_FORMAT_GZIP_UNPACK || fmt==EY_ZLIB_FORMAT_DEFLATE_UNPACK)
+		inflateEnd(sp);
+	mm->free(o_buf);
+	mm->free(i_buf);
+	ey_zfree(zlib_slab, priv_data);
+}
+
+const char *ey_zlib_errstr(ey_zlib_t z)
+{
+	ey_zlib_private_t *priv_data = (ey_zlib_private_t*)z;
+	
+	if(!priv_data)
+		return "ey_zlib create failed\n";
+	if(!priv_data->err_msg)
+		return "no error found\n";
+	return priv_data->err_msg;
+}
+
+int ey_zlib_pack(ey_zlib_t z, char *i_buf, size_t i_len, ey_zlib_callback cb, void *arg)
+{
+	/*TODO:*/
+	return 0;
+}
+
+int ey_zlib_unpack(ey_zlib_t z, char *i_buf, size_t i_len, ey_zlib_callback cb, void *arg)
+{
+	/*TODO:*/
+	return 0;
+}
